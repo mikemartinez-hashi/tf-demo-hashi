4c974812895e1dc8046365fcf87b008893ac886e
mikemartinez-hashi <mike.martinez@hashicorp.com>
2025-04-30 17:55:27 -0500

    updated outputs

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,7 @@ output "instance_id" {
 output "security_group_id" {
   value = aws_security_group.allow_ssh_and_http.id
 }
+
+output "security_group_name" {
+  value = aws_security_group.allow_ssh_and_http.name
+}
