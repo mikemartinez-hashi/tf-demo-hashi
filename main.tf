6f6fdcad026bae89a2b33e0642e547e07fd01768
mikemartinez-hashi <mike.martinez@hashicorp.com>
2025-05-21 11:54:16 -0500

    deleting infra

--- main.tf
+++ main.tf
@@ -65,6 +65,13 @@ resource "aws_security_group" "allow_ssh_and_http" {
     cidr_blocks = ["0.0.0.0/0"] # Allow HTTP from anywhere (for demonstration purposes, restrict this in production)
   }
 
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow HTTP from anywhere (for demonstration purposes, restrict this in production)
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
